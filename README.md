bfe7482194e6208771d489bfaf3cc5df4b8be0c9
Sandro Baccega <sandro.baccega@smc.it>
2019-12-19 22:45:48 +0100

    First commit :tada:

---
+++ README.md
@@ -0,0 +1,81 @@
+Sandro Baccega(865024)
+
+Arnaldo Santoro (822274)
+
+Due date: 23/12/2019
+
+## REQUIREMENT SPECIFICATION
+
+### Assignment 2: Einstein's notation
+
+Augment the tensor library with the ability to perform tensor operations using Einstein's notation.
+
+Einstein's notation, introduced in 1916 to simplify the notation used for general relativity, expresses tensor operations by rendering implicit the summation operations. According to this notation, repeated indexes in a tensorial expression are implicitly summed over, so the expression
+
+a_ijk b_j
+
+represents a rank 2 tensor c indexed by i and k such that
+
+c_ik = Σj a_ijk b_j
+
+The notation allows for simple contractions
+
+Tr(a) = a_ii
+
+as well as additions (subtractions) and multiplications.
+
+
+
+The operations should work with dynamically ranked tensors as well as fixed-rank tensors by maintaining and checking as much static information as possible.
+
+
+
+**Hint**: define index types. indexing operations with index types should return proxy objects that allow deferred operations and conversion to the correct tensor type.
+
+
+
+## USAGE
+
+### Examples
+
+#### Trace
+
+#### Multiplication
+
+#### Conversion
+
+#### Combinations
+
+#### Generalized Kronecker Product
+
+The operation OP between two tensors A and B or rank r and s with no common indexes returns a tensor of rank r*s whose ranks' elements are the result of OP between each element of A and each element of the rank of B .
+
+This results similar to a Kronecker product which stores the results in different ranks of a tensor.
+
+If there wasn't a bug in the flatten operation one could easily implement a Kronecker product exploiting this functionality.
+
+
+
+## DESIGN
+
+### Composite Pattern
+
+We thought of the Composite Pattern to implement tensor operations in a uniform way.
+
+This is obtained through different implementation of the function `evaluate()`, which behaves differently for each class implementing the operations.
+
+### Strategy Pattern
+
+A definition of the _strategy pattern_ is a reusable solution that lets an algorithm vary independently from clients that use it; in our code we use this solution to implement the different operations in the implicit summation of Einstein's formalism; this application is combined with the template system to obtain a _static strategy pattern_ implementation.
+
+The object `tensor_op` is forward-declared and consists of the _functor_ applying the algorithm.
+
+The following `struct`s are "functors" (in a broader sense) implementing the operations between two elements of type T, which are then placed in the specialized template of the definition for the `operator+`, and `operator*` respectively.
+
+
+
+## Bugs
+
+A combination of contraction and other operations with shared free indexes break the program.
+
+This happens because the tensors are evaluated right out when they are passed to objects.(...)
---
+++ main.cc
@@ -0,0 +1,491 @@
+#include <chrono>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include "Tensor.hpp"
+
+// --- Utils
+// fills tensor with index values
+template <typename T>
+
+void fill_tensor_i(T &tensor, int i = 0, int m = 1)
+{
+
+  for (auto it = tensor.begin(); it != tensor.end(); ++it)
+  {
+
+    *it = i * m;
+
+    i++;
+  }
+}
+
+// fills tensor with a constant c
+template <typename T>
+
+void fill_tensor_c(T &tensor, int c = 1)
+{
+
+  for (auto it = tensor.begin(); it != tensor.end(); ++it)
+  {
+
+    *it = c;
+  }
+}
+
+template <class TensorType>
+void printTensor(const TensorType &t, std::ostream &stream = std::cout)
+{
+
+  for (auto iterator = t.begin(); iterator != t.end(); iterator++)
+  {
+
+    stream << *iterator << ", ";
+  }
+
+  stream << std::endl;
+}
+
+template <class TensorType>
+void assertTensorValues(const TensorType &tensor, std::string expected)
+{
+
+  std::stringstream buffer;
+
+  printTensor(tensor, buffer);
+
+  assert(buffer.str().compare(expected) == 0);
+}
+
+// --- Unit Tests
+
+void tensorMultiplicationTests()
+{
+
+  std::cout << "Tensor Multiplication Tests:" << std::endl;
+
+  tensor::tensor<int> a(4, 3, 4);
+
+  fill_tensor_i(a, 0);
+
+  tensor::tensor<int> b(3);
+
+  fill_tensor_i(b, 0);
+
+  auto exp = a.ein("ijk") * b.ein("j");
+
+  tensor::tensor<int> c = exp.evaluate();
+
+  printTensor(c);
+
+  assertTensorValues(c,
+
+                     "20, 23, 26, 29, 56, 59, 62, 65, 92, 95, 98, 101, 128, "
+
+                     "131, 134, 137, \n");
+
+  std::cout << std::endl;
+
+  tensor::tensor<int> a1(4);
+
+  fill_tensor_i(a1, 0);
+
+  tensor::tensor<int> b1(4);
+
+  fill_tensor_i(b1, 0);
+
+  auto exp2 = a1.ein("i") * b1.ein("i");
+
+  tensor::tensor<int> c1 = exp2.evaluate();
+
+  printTensor(c1);
+
+  assertTensorValues(c1, "14, \n");
+
+  std::cout << std::endl;
+
+  tensor::tensor<int> a2(4, 4);
+
+  fill_tensor_i(a2, 0);
+
+  tensor::tensor<int> b2(4, 4);
+
+  fill_tensor_i(b2, 0);
+
+  tensor::tensor<int> c2(4);
+
+  fill_tensor_i(c2, 0);
+
+  auto exp3 = a2.ein("ij") * b2.ein("ik");
+
+  tensor::tensor<int> d2 = exp3.evaluate();
+
+  printTensor(d2);
+}
+
+void sameTensorMultiplicationTests()
+{
+
+  std::cout << "Same Tensor Multiplication Test:" << std::endl;
+
+  tensor::tensor<int> a(3);
+
+  fill_tensor_i(a, 0);
+
+  auto exp = a.ein("i") * a.ein("i");
+
+  tensor::tensor<int> c = exp.evaluate();
+
+  printTensor(c);
+
+  assertTensorValues(c, "5, \n");
+
+  tensor::tensor<int> d(3, 3);
+
+  fill_tensor_i(d, 0);
+
+  auto exp2 = d.ein("ij") * d.ein("ij");
+
+  tensor::tensor<int> e = exp2.evaluate();
+
+  printTensor(e);
+
+  assertTensorValues(e, "204, \n");
+
+  std::cout << std::endl;
+}
+
+void traceTests()
+{
+
+  std::cout << "Trace Tests:" << std::endl;
+
+  tensor::tensor<int> f(3, 3);
+
+  fill_tensor_i(f, 0);
+
+  auto exp3 = f.ein("ii");
+
+  tensor::tensor<int> g = exp3.evaluate();
+
+  printTensor(g);
+
+  assertTensorValues(g, "12, \n");
+
+  tensor::tensor<int> h(3, 3, 3);
+
+  fill_tensor_i(h, 0);
+
+  auto exp4 = h.ein("iii");
+
+  tensor::tensor<int> i = exp4.evaluate();
+
+  printTensor(i);
+
+  assertTensorValues(i, "39, \n");
+
+  std::cout << std::endl;
+}
+
+void tensorAdditionTests()
+{
+
+  std::cout << "Tensor Addition Tests:" << std::endl;
+
+  tensor::tensor<int> a(4);
+
+  fill_tensor_i(a, 0);
+
+  tensor::tensor<int> b(4);
+
+  fill_tensor_i(b, 0);
+
+  auto exp = a.ein("i") + b.ein("i");
+
+  tensor::tensor<int> c = exp.evaluate();
+
+  printTensor(c);
+
+  assertTensorValues(c, "12, \n");
+
+  tensor::tensor<int> a1(2, 2, 2);
+
+  fill_tensor_i(a1, 0);
+
+  tensor::tensor<int> b1(2, 2, 2);
+
+  fill_tensor_i(b1, 0);
+
+  auto exp1 = a1.ein("ijk") + b1.ein("ijk");
+
+  tensor::tensor<int> c1 = exp1.evaluate();
+
+  printTensor(c1);
+
+  assertTensorValues(c1, "56, \n");
+
+  std::cout << std::endl;
+}
+
+void operationConcatTests()
+{
+
+  std::cout << "Tensor Operation Concat Tests:" << std::endl;
+
+  tensor::tensor<int> a(2, 2);
+
+  fill_tensor_i(a, 0);
+
+  tensor::tensor<int> b(2, 2);
+
+  fill_tensor_i(b, 0);
+
+  tensor::tensor<int> c(2);
+
+  fill_tensor_i(c, 0);
+
+  auto exp = a.ein("ij") * b.ein("ik") * c.ein("j");
+
+  tensor::tensor<int> d = exp.evaluate();
+
+  printTensor(d);
+
+  // tensor::tensor<int> a(4);
+
+  // fill_tensor_i(a, 0);
+
+  // tensor::tensor<int> b(4);
+
+  // fill_tensor_i(b, 0);
+
+  // tensor::tensor<int> c(4);
+
+  // fill_tensor_i(c, 0);
+
+  // auto exp = a.ein("i") * b.ein("i") * c.ein("i");
+
+  // tensor::tensor<int> d = exp.evaluate();
+
+  // printTensor(d);
+
+  // assertTensorValues(d, "12, \n");
+
+  // tensor::tensor<int> a1(2,2,2);
+
+  // fill_tensor_i(a1, 0);
+
+  // tensor::tensor<int> b1(2,2,2);
+
+  // fill_tensor_i(b1, 0);
+
+  // auto exp1 = a1.ein("ijk") + b1.ein("ijk");
+
+  // tensor::tensor<int> c1 = exp1.evaluate();
+
+  // printTensor(c1);
+
+  // assertTensorValues(c1, "56, \n");
+
+  std::cout << std::endl;
+}
+
+void mixedConcatTests()
+{
+
+  std::cout << "Tensor Mixed Operation Concat Tests:" << std::endl;
+
+  tensor::tensor<int> a(2, 2);
+
+  fill_tensor_i(a, 0);
+
+  tensor::tensor<int> b(2, 2);
+
+  fill_tensor_i(b, 0);
+
+  tensor::tensor<int> c(2, 2);
+
+  fill_tensor_c(c, 1000);
+
+  auto exp = a.ein("ij") * b.ein("jk") + c.ein("kl");
+
+  tensor::tensor<int> d = exp.evaluate();
+
+  printTensor(d);
+
+  std::cout << std::endl;
+}
+
+void tensorConcatenation()
+{
+
+  std::cout << "Concatenatation example:" << std::endl;
+
+  tensor::tensor<int> a(3);
+
+  fill_tensor_i(a, 0);
+
+  tensor::tensor<int> b(3);
+
+  fill_tensor_i(b, 0, 100);
+
+  auto exp = a.ein("i") + b.ein("j");
+
+  tensor::tensor<int> d = exp.evaluate();
+
+  printTensor(d);
+
+  std::cout << "The rank of the new tensor is " << d.get_rank() << std::endl
+            << std::endl;
+}
+
+void KroneckerProduct()
+{
+
+  std::cout << "Kronecker example from wikipedia:" << std::endl;
+
+  tensor::tensor<int> a(2, 2);
+
+  a(0, 0) = 1;
+
+  a(0, 1) = 2;
+
+  a(1, 0) = 3;
+
+  a(1, 1) = 1;
+
+  tensor::tensor<int> b(2, 2);
+
+  b(0, 0) = 0;
+
+  b(0, 1) = 3;
+
+  b(1, 0) = 2;
+
+  b(1, 1) = 1;
+
+  auto exp = a.ein("ij") * b.ein("kl");
+
+  tensor::tensor<int> d = exp.evaluate(); //.flatten(0,2);
+
+  printTensor(d);
+
+  std::cout << "The rank of the new tensor is " << d.get_rank() << std::endl;
+
+  std::cout << "The operation returns a tensor of rank 2*2, but the elements are multiplied correctly.\n"
+
+            << "If there wasn't a bug in the flatten operation one could easily implement a Kronecker product exploiting this functionality"
+
+            << std::endl
+            << std::endl;
+}
+
+void numberOfIndexesCheck()
+{
+
+  std::cout << "This fails an assert (mismatch between number of indexes and dimensions)" << std::endl;
+
+  tensor::tensor<int> a(2, 2);
+
+  fill_tensor_i(a, 0);
+
+  tensor::tensor<int> b(2);
+
+  fill_tensor_i(b, 0);
+
+  auto exp = a.ein("ij") * b.ein("jk");
+
+  tensor::tensor<int> d = exp.evaluate();
+
+  printTensor(d);
+
+  std::cout << "Shouldn't work. Nothing gets printed." << std::endl;
+
+  std::cout << std::endl;
+}
+
+void invalidTrace()
+{
+
+  std::cout << "This fails an assert (dimensions of different size have same index)" << std::endl;
+
+  tensor::tensor<int> a(2, 3);
+
+  fill_tensor_i(a, 0);
+
+  auto exp = a.ein("ii");
+
+  auto d = exp.evaluate();
+
+  printTensor(d);
+
+  std::cout << "Shouldn't work dynamically." << std::endl;
+
+  std::cout << std::endl;
+}
+
+void breaksComputation()
+{
+
+  std::cout << "This also should break the code" << std::endl;
+
+  auto a = tensor::tensor<int>(3, 4);
+
+  fill_tensor_i(a, 0);
+
+  auto b = tensor::tensor<int>(2, 2);
+
+  fill_tensor_i(b, 0);
+
+  auto exp2 = a.ein("ij") * b.ein("jk");
+
+  auto d = exp2.evaluate();
+
+  printTensor(d);
+
+  std::cout << "Shouldn't work at all." << std::endl
+            << std::endl;
+
+  std::cout << "A combination of contraction and other operations with shared free indexes break the program" << std::endl;
+
+  a = tensor::tensor<int>(2, 2);
+
+  b = tensor::tensor<int>(2, 2);
+
+  fill_tensor_i(a, 0);
+
+  fill_tensor_i(b, 0);
+
+  auto exp3 = a.ein("ii") + b.ein("ij");
+
+  auto res = exp3.evaluate();
+
+  printTensor(res);
+
+  std::cout << std::endl;
+}
+
+// --- Main
+
+int main()
+{
+
+  tensorMultiplicationTests();
+
+  sameTensorMultiplicationTests();
+
+  traceTests();
+
+  tensorAdditionTests();
+
+  operationConcatTests();
+
+  mixedConcatTests();
+
+  tensorConcatenation();
+
+  KroneckerProduct();
+
+  breaksComputation();
+
+  return 0;
+}
